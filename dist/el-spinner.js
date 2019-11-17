efc8bb03440f2c29a74e42a963825b9e9d2ea8e0
fralonra <fralonra@aliyun.com>
2019-11-17 13:50:06 +0800

    bumped v0.2.6

--- dist/el-spinner.js
+++ dist/el-spinner.js
@@ -2,7 +2,7 @@
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
   (global = global || self, global.ElSpinner = factory());
-}(this, function () { 'use strict';
+}(this, (function () { 'use strict';
 
   var spinners = {
     dots: {
@@ -994,4 +994,4 @@
 
   return Spinner;
 
-}));
+})));
